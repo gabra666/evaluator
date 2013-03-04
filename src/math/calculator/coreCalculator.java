270effde755bd5ed06892879e34d680435042391
Bryan <Bryan@192.168.1.218>
2013-03-04 22:19:27 +0100

    coreCalculator que se quedo por fuera del Commit ¬¬

--- src/math/calculator/coreCalculator.java
+++ src/math/calculator/coreCalculator.java
@@ -1,5 +1,45 @@
 package math.calculator;
 
+import math.annotation.Operation;
+
 public class coreCalculator implements NumberCalculator{
+
+    public coreCalculator() {
+    }
+    
+    @Operation("+")
+    public Double add(Double p0, Double p1){
+        return p0+p1;
+    }
+    @Operation("+")
+    public Double add(Double p0, Integer p1){
+        return p0+p1;
+    }
+    @Operation("+")
+    public Double add(Integer p0, Double p1){
+        return p0+p1;
+    }
+    @Operation("+")
+    public Integer add(Integer p0, Integer p1){
+        return p0+p1;
+    }
+    @Operation("-")
+    public Double substract(Double p0, Double p1){
+        return p0-p1;
+    }
+    @Operation("-")
+    public Double substract(Double p0, Integer p1){
+        return p0-p1;
+    }
+    @Operation("-")
+    public Double substract(Integer p0, Double p1){
+        return p0-p1;
+    }
+    @Operation("-")
+    public Integer substract(Integer p0, Integer p1){
+        return p0-p1;
+    }
+    
     
 }
+ 
